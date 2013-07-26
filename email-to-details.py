651dca272c5bc80ee868804bde47bcd7dca6d6e4
hancongcs2c <cong.han@bj.cs2c.com.cn>
2013-07-26 09:04:22 +0800

    email-to-details.py

---
+++ email-to-details.py
@@ -0,0 +1,21 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Find infomation by email address')
+
+parser.add_argument('emails', metavar='EMAIL', nargs='+',
+                    help='An email address to query')
+
+args = parser.parse_args()
+
+from launchpadlib.launchpad import Launchpad
+
+launchpad = Launchpad.login_with('openstack-dm', 'production')
+
+print  ' Name, karma,  email,    date,  link'
+for email in args.emails:
+    try:
+        person = launchpad.people.getByEmail(email=email)
+        if person:
+            print person.name, person.karma,  email,     person.date_created.date(),    person.web_link
+    except:
+        continue
